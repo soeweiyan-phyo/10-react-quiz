55dbe4ff3cd6e2b904dc4f16aa90c3af55b6bb92
Soe Wei Yan Phyo <38011380+soeweiyan-phyo@users.noreply.github.com>
2023-08-20 19:32:05 +1000

    Handle new answers

--- src/components/Options.js
+++ src/components/Options.js
@@ -1,8 +1,21 @@
-function Options({ question }) {
+function Options({ question, dispatch, answer }) {
+  const hasAnswer = answer !== null;
+
   return (
     <div className="options">
-      {question.options.map((option) => (
-        <button className="btn btn-option" key={option}>
+      {question.options.map((option, index) => (
+        <button
+          className={`btn btn-option ${index === answer ? "answer" : ""} ${
+            hasAnswer
+              ? index === question.correctOption
+                ? "correct"
+                : "wrong"
+              : ""
+          }`}
+          key={option}
+          disabled={hasAnswer}
+          onClick={() => dispatch({ type: "newAnswer", payload: index })}
+        >
           {option}
         </button>
       ))}
